56f66a6b426310e432be29eeced277ce02270289
rrybasouleymane <rrybasouleyman98@gmail.com>
2026-03-25 11:57:05 +0000

    Update EmployeRepository.java

--- src/main/java/com/ipiecoles/java/java350/repository/EmployeRepository.java
+++ src/main/java/com/ipiecoles/java/java350/repository/EmployeRepository.java
@@ -7,11 +7,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeRepository extends JpaRepository<Employe, Long> {
+
     @Query("select max(substring(matricule,2)) from Employe")
     String findLastMatricule();
 
     Employe findByMatricule(String matricule);
 
-    @Query("select avg(performance) from Employe where SUBSTRING(matricule,0,1) = ?1 ")
+    @Query("select avg(performance) from Employe where SUBSTRING(matricule,0,1) = ?1")
     Double avgPerformanceWhereMatriculeStartsWith(String premiereLettreMatricule);
 }
